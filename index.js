201d3bda975134d7a4d3e18ee4ae26a54ee7094e
unknown <devanshraghav2000@gmail.com>
2024-01-02 18:10:20 +0530

    work complete

--- index.js
+++ index.js
@@ -2,14 +2,53 @@ const cardName = document.getElementById("card-name");
 const cardNumber = document.getElementById("card-number");
 const expiryMonth = document.getElementById("exp-month");
 const expiryYear = document.getElementById("exp-year");
+const cvc = document.getElementById("cvc");
 
-function validateData() {}
+let validData = false;
+
+document.getElementById("form-submit").addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  validateData();
+
+  if (validData) {
+    document.getElementById("form-submit").style.display = "none";
+    document.querySelector(".complete-state").style.display = "flex";
+  }
+});
+
+function validateData() {
+  EmptyFieldCheck(cardName);
+  EmptyFieldCheck(cardNumber);
+  month_year_EmptyFieldCheck(expiryMonth);
+  month_year_EmptyFieldCheck(expiryYear);
+  EmptyFieldCheck(cvc);
+  cardExpiryMonthInput(expiryMonth);
+  if (validData) {
+    checkCardNumberLength();
+  }
+}
 
 function setError(element, message) {
   const parent = element.parentElement;
   const errorDisplay = parent.querySelector(".error");
 
+  element.classList.add("errorBorder");
+
   errorDisplay.innerText = message;
+
+  validData = false;
+}
+
+function setMonthYearExpiryError(element,message){
+  const parent = element.parentElement.parentElement;
+  const errorDisplay = parent.querySelector(".error");
+
+  element.classList.add("errorBorder");
+
+  errorDisplay.innerText = message;
+
+  validData = false;
 }
 
 function removeError(element) {
@@ -17,13 +56,34 @@ function removeError(element) {
   const errorDisplay = parent.querySelector(".error");
 
   errorDisplay.innerText = "";
+  element.classList.remove("errorBorder");
+  validData = true;
+}
+
+function removeMonthYearError(element) {
+  const parent = element.parentElement.parentElement;
+
+  const errorDisplay = parent.querySelector(".error");
+
+  errorDisplay.innerText = "";
+  element.classList.remove("errorBorder");
+  validData = true;
+  
+  
 }
 
 function EmptyFieldCheck(target) {
   let data = target.value;
 
-  if ((data = "")) {
-    setError(target, "Field required");
+  if (data.length == 0) {
+    setError(target, "Can't be blank");
+  }
+}
+function month_year_EmptyFieldCheck(target) {
+  let data = target.value;
+
+  if (data.length == 0) {
+    setMonthYearExpiryError(target, "Can't be blank");
   }
 }
 
@@ -33,53 +93,90 @@ function cardNameInput(element) {
   // let cardNameValue = element.value
   cardName.innerText = element.value;
 
-  if (cardName.innerText.length == 0){
+  if (cardName.innerText.length == 0) {
     cardName.innerText = "Test Name";
+    setError(element, "Can't be blank");
+  } else {
+    removeError(element);
   }
 }
 
 function cardNumberInput(element) {
   let cardNumbervalue = document.querySelector(".card-num");
-  let cardNum = element.value.replace(/\D/g, "");
+
+  let numbers = /^[0-9]+$/;
+  let cardNum = element.value;
   if (cardNum.length > 0) {
     cardNum = cardNum.slice(0, 16);
     cardNum = cardNum.match(/.{1,4}/g).join(" ");
   }
-  element.value = cardNum;
+
+  if (!numbers.test(element.value)) {
+    setError(element, "Wrong Format");
+  } else {
+    removeError(element);
+  }
   cardNumbervalue.innerText = cardNum;
-  if (cardNumbervalue.innerText.length == 0){
+  if (cardNumbervalue.innerText.length == 0) {
     cardNumbervalue.innerText = "0000 0000 0000 0000";
+    setError(element, "Can't be blank");
   }
 }
 
 function cardExpiryMonthInput(element) {
-  let expiryDate = document.querySelector('.month')
+  let expiryDate = document.querySelector(".month");
   let elementData = element.value.replace(/\D/g, "");
   element.value = elementData;
   expiryDate.innerText = elementData;
 
-  if (expiryDate.innerText.length == 0){
+  if (expiryDate.innerText.length == 0) {
     expiryDate.innerText = "00";
+  }
+  if (element.value != "") {
+    if (!(element.value >= 1 && element.value <= 12)) {
+      setMonthYearExpiryError(element, "Wrong Format");
+    } else {
+      removeMonthYearError(element);
+    }
   }
 }
 
 function cardExpiryYearInput(element) {
-  let expiryYear = document.querySelector('.year')
+  removeMonthYearError(element)
+  let expiryYear = document.querySelector(".year");
   let elementData = element.value.replace(/\D/g, "");
   element.value = elementData;
   expiryYear.innerText = elementData;
 
-  if (expiryYear.innerText.length == 0){
+  if (expiryYear.innerText.length == 0) {
     expiryYear.innerText = "00";
   }
 }
 
 function cvcInput(element) {
-  let cvcNumber = document.querySelector('.cvc-number');
+  let cvcNumber = document.querySelector(".cvc-number");
   let elementData = element.value.replace(/\D/g, "");
   element.value = elementData;
   cvcNumber.innerText = elementData;
-  if (cvcNumber.innerText.length == 0){
+  if (cvcNumber.innerText.length == 0) {
     cvcNumber.innerText = "000";
+  } else {
+    removeError(element);
   }
 }
+
+function checkCardNumberLength() {
+  if (cardNumber.value.length < 16) {
+    setError(cardNumber, "Should be at least 16 numbers");
+  }
+}
+
+cardName.addEventListener("blur", (e) => EmptyFieldCheck(e.srcElement));
+cardNumber.addEventListener("blur", (e) => EmptyFieldCheck(e.srcElement));
+expiryMonth.addEventListener("blur", (e) =>
+  month_year_EmptyFieldCheck(e.srcElement)
+);
+expiryYear.addEventListener("blur", (e) =>
+  month_year_EmptyFieldCheck(e.srcElement)
+);
+cvc.addEventListener("blur", (e) => EmptyFieldCheck(e.srcElement));
